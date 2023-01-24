afc941657de0dd856832d610397808debcfc0788
Eugenio Nunes <eugenio.programmer@gmail.com>
2023-01-24 08:48:43 -0300

    Adicionando o arquivo ShowProfileService

--- src/config/mail/HandlebarsMailTemplate.ts
+++ src/config/mail/HandlebarsMailTemplate.ts
@@ -1,20 +1,25 @@
 import Handlebars from "handlebars";
+import fs from 'fs';
 
 interface ITemplateVariable {
   [key: string]: string | number;
 }
 
 interface IParseMailTemplate {
-  template: string;
+  file: string;
   variables: ITemplateVariable
 }
 
 export default class HandlebarsMailTemplate {
   public async parse({
-    template,
+    file,
     variables
   }: IParseMailTemplate): Promise<string> {
-    const parseTemplate = Handlebars.compile(template)
+
+    const templateFileContent = await fs.promises.readFile(file, {
+      encoding: 'utf-8'
+    })
+    const parseTemplate = Handlebars.compile(templateFileContent)
 
     return parseTemplate(variables)
   }
